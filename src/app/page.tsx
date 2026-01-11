f0585bee0cb6acaa295060566cde3e8eaa3bc159
edwardbudaza <edward.budaza@yahoo.co.za>
2026-01-11 02:08:22 +0200

    TTRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import prisma from "@/lib/db";
+"use client"
 
-const HomePage = async () => {
-  const users = await prisma.user.findMany();
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
+
+const HomePage =  () => {
+  const trpc = useTRPC()
+  const {data: users} = useQuery(trpc.getUsers.queryOptions())
+  
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       {JSON.stringify(users)}
